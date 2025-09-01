7384837213ae3df125e77719e2f3f12fb42cad3d
Hạnh Mai <hanhmai120997@gmail.com>
2025-09-02 01:59:10 +0700

    Add files via upload

---
+++ Assignments_DMS/SQL-01_StrongHold/session02_intro2.sql
@@ -0,0 +1,19 @@
+﻿/*chú thích nhiều dòng*/
+/*1.chú thích
+	-- single line
+	/**/multiple line
+2.chọn current database (DB mặc định)
+3.thực thi câu lệnh dc chọn
+4.khác*/
+use StrongHold
+go
+--cau 2
+--1.CCode, CName from Customer
+select CCode, CName
+from Customer
+--2.all field from OrderMaster
+select *
+from OrderMaster
+--3.OrderNo, ItemCode,Quantity from OrderDetails
+select OrderNo,ItemCode, Qty
+from OrderDetails
