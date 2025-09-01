4883fdc28c8a42a9b0a2935554b4638ec2e29321
Hạnh Mai <hanhmai120997@gmail.com>
2025-09-02 01:47:37 +0700

    Add files via upload

---
+++ Lesson02_Entity/session02_intro2 - prepareLab02.sql
@@ -0,0 +1,33 @@
+﻿--5.field alias
+use StrongHold
+go
+--tại sao 2 cái viết khác nhau nhưng ra kết quả giống nhau
+select CCode,CName
+from Customer
+
+select Customer.CCode, Customer.CName
+from Customer
+--alias là gì?vì sao cần sd
+select A.CCode, A.Cname
+from Customer as A--có thể viết tắt from Customer A
+	--alias dùng để chỉ cụ thể cột CCode được lấy từ bảng nào nếu select đang chọn nhiều bảng, có nhiều cột giống nhau
+
+--6.return a result --> bôi từng bó lệnh-> f5 để xem khác biệt
+	--lọc giá trị của cột ccode
+select CCode
+from Customer
+	--tìm giá trị TLT trong cột ccode
+select CCode
+from Customer
+where CCode ='TLT'
+	--ccode nằm trong '' được xem là hằng
+select 'CCode'
+from Customer
+	--tìm giá trị TLT trong cột ccode, nếu đúng-> hiển thị true
+select 'true'
+from Customer
+where CCode ='TLT'
+	--tìm giá trị TLT1 trong cột ccode, nếu đúng-> hiển thị true
+select 'true'
+from Customer
+where CCode ='TLT1'
---
+++ Lesson02_Entity/session02_intro2.sql
@@ -0,0 +1,20 @@
+﻿/*chú thích nhiều dòng*/
+/*1.chú thích
+	-- single line
+	/**/multiple line
+2.chọn current database (DB mặc định)
+3.thực thi câu lệnh dc chọn
+4.khác
+để tắt Outpanel: ctrt + R*/
+use StrongHold
+go
+--cau 2
+--1.CCode, CName from Customer
+select CCode, CName
+from Customer
+--2.all field from OrderMaster
+select *
+from OrderMaster
+--3.OrderNo, ItemCode,Quantity from OrderDetails
+select OrderNo,ItemCode, Qty
+from OrderDetails
